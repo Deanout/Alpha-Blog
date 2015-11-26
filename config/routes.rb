66cb89f559f210897324ba5d61651670a9f7589b
deanout <dean@geminigames.co>
2015-11-26 18:11:43 +0000

    Auto Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
+  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
